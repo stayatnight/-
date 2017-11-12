2b5df86a02d88ed189b3e39879c0416562463dd6
stayatnight <2234024575@qq.com>
2017-11-12 18:25:44 +0800

    完成序号查找，未改错版

--- Project1/linked List.cpp
+++ Project1/linked List.cpp
@@ -27,7 +27,7 @@ LNode*creat_LinkList()
 	return(head);
 }
 //头插法创建单链表
-void print(LNode*head)
+/*void print(LNode*head)
 {
 	LNode*p;
 	p = head->next;
@@ -38,22 +38,32 @@ void print(LNode*head)
 		printf("%d\n", p->data);
 	p=p->next;//曾经在这犯了猪一样的错误。。。
 	}
-}
+}*/
 //遍历单链表并输出
 int Get_Elem(int i,LNode*head)
 {
-
-
-
-
-
+	LNode*p;
+	p = head->next;
+	if (i > head->data || i <= 0)
+		return NULL;
+	for ( int a = 0;  a <i;  a++)
+	{
+		p = p->next;
+		if (a==i-1)
+			printf("查找的值是%d", p->data);
+	}
+	return true;
 }
 //按序号查找
 int main()
 {
+	int i;
 	LNode*head;
 	head = creat_LinkList();//调用函数并接收返回值
-	print(head);
+	//print(head);
+	printf("请输入要查找值的序号？");
+	scanf_s("%d", &i);
+	Get_Elem(i, head);
 	system("pause");
 	return 0;
 }
